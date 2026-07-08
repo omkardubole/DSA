4917e538fefce9f6ccc09abd7976f6ea58f80a33
omkardubole <omkardubole46@gmail.com>
2026-07-08 11:33:27 +0530

    Time: 0 ms (100%), Space: 8.4 MB (83.86%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverse(int x) { 
+        long long rev = 0;
+        while(x!=0) {
+        long long lastDigit = x % 10;
+        x = x / 10;
+        rev = (rev * 10) + lastDigit;
+        } if (rev > INT_MAX || rev < INT_MIN)
+            return 0;
+        return (int)rev;
+    }
+};
