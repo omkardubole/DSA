95ce377429b9389eaee25319b4a189fe7061e2ee
omkardubole <omkardubole46@gmail.com>
2026-08-06 10:27:16 +0530

    Time: 4 ms (17.79%), Space: 97.4 MB (25.95%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -5,7 +5,8 @@ public:
         int maxProfit = 0;
         int n = prices.size();
         for(int i=0; i<n; i++) {
-            int profit  = prices[i] - mini;
+            // profit(agar mai aaj bheju toh kitne ka fayda hoga)           = (aaj ka prices kya hai) - (maine kitne me kharida tha[jo ki mini me store hoga]) 
+            int profit  = prices[i] - mini;  
             maxProfit = max(maxProfit, profit);
             mini = min(mini, prices[i]);
         }
