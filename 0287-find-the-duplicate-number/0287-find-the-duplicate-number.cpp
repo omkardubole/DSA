d3a07d121df7448b8239e1a4d3975a2b924aa987
omkardubole <omkardubole46@gmail.com>
2026-08-08 19:52:59 +0530

    Time: 7 ms (50.51%), Space: 65 MB (91.21%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+
+        // Phase 1: Detect cycle
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while(slow != fast);
+
+        // Phase 2: Find entrance of cycle
+        slow = nums[0];
+
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+};
