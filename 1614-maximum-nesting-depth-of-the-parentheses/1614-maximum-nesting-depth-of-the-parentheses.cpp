fbce8e47d5f2b9401c145671e28e9e9d165ca7f3
omkardubole <omkardubole46@gmail.com>
2026-09-09 16:08:05 +0530

    Time: 0 ms (100%), Space: 8.4 MB (24.37%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int openBrackets = 0;
+        int result = 0;
+
+        for(char &ch : s) {
+            if(ch == '(') {
+                openBrackets++;
+            }
+            else if(ch == ')') {
+                openBrackets--;
+            }
+            result = max(result, openBrackets);
+        }
+        return result;
+    }
+};
