bfbdb1f542cff43b62ba1502af8c471f3ab2d337
omkardubole <omkardubole46@gmail.com>
2026-09-09 11:36:44 +0530

    Time: 0 ms (100%), Space: 8.5 MB (84.7%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
     int countCommas(int n) {
-      return n < 1000 ? 0 : n-999;  
-    }
+      return n < 1000 ? 0 : max(n-999, 0);  //negative jaa sakta hai isiliye max of n-999 and 0 kiya hai
+    }                                       //taaki negative naa jaaye 
 };
