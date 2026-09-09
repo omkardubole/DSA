07f1dcaef3de5824942dfa552a9322ad7967a162
omkardubole <omkardubole46@gmail.com>
2026-09-09 19:38:08 +0530

    Time: 190 ms (26.68%), Space: 72.3 MB (24.83%) - LeetHub

---
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    unordered_map<string, bool> mp;
+
+    bool isConcatenated(string word, unordered_set<string>& st) {
+
+        if(mp.find(word) != mp.end()) {
+            return mp[word];
+        }
+
+        int l = word.size();
+
+        for(int i = 0; i < l - 1; i++) {
+
+            string prefix = word.substr(0, i + 1);
+            string suffix = word.substr(i + 1);
+
+            if(st.find(prefix) != st.end() && isConcatenated(suffix, st) || 
+               st.find(prefix) != st.end() && st.find(suffix) != st.end()) {
+
+                return mp[word] = true;
+            }
+        }
+
+        return mp[word] = false;
+    }
+
+    vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+
+        unordered_set<string> st(words.begin(), words.end());
+
+        vector<string> result;
+
+        for(string word : words) {
+
+            st.erase(word);
+
+            if(isConcatenated(word, st)) {
+                result.push_back(word);
+            }
+
+            st.insert(word);
+        }
+
+        return result;
+    }
+};
