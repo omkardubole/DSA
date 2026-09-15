0df90a27d5dc42225b7b43ee3c09576ec05630d8
omkardubole <omkardubole46@gmail.com>
2026-09-15 15:19:47 +0530

    Time: 0 ms (100%), Space: 8.2 MB (26.94%) - LeetHub

---
+++ 0880-decoded-string-at-index/0880-decoded-string-at-index.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string decodeAtIndex(string s, int k) {
+        int n = s.size();
+
+        long long size = 0;
+
+        for(char ch : s) {
+            if(isdigit(ch)) {
+                size = size*(ch - '0');
+            }
+            else {
+                size++;
+            }
+            
+        }
+
+        for(int i=n-1; i>=0; i--) {
+            k = k%size;
+
+            if(k == 0 && isalpha(s[i])) {
+                return string(1, s[i]);
+            }
+
+            if(isalpha(s[i])) {
+                size--;
+            }
+            else{
+                size = size/(s[i]-'0');
+            }
+        }
+        return "";
+    }
+};
