f093c0d5e21aa81242d421d1a2457bc561c8b832
omkardubole <omkardubole46@gmail.com>
2026-09-17 19:34:24 +0530

    Time: 0 ms (100%), Space: 16.5 MB (17.42%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+    stringstream ss(s);
+
+    string token = "";
+    string result = "";
+
+    while(ss >> token) {
+
+        reverse(token.begin(), token.end());
+        result += token  + " ";
+    }
+
+    return result.substr(0, result.size() - 1);
+    }
+};
